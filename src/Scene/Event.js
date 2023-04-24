3656081a2d2063e4778d24a890710818e074b264
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-04-24 14:21:42 +0900

    modified 1. coming soon page NFT 2. coming soon page EVENT

--- src/Scene/Event.js
+++ src/Scene/Event.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: whitesmoke;
+`;
 
 function Event() {
-  return <div>Event</div>;
+  return <Container>Coming soon</Container>;
 }
 
 export default Event;
--- src/Scene/Nft.js
+++ src/Scene/Nft.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: whitesmoke;
+`;
 
 function Nft() {
-  return <div>Nft</div>;
+  return <Container>Coming soon</Container>;
 }
 
 export default Nft;
